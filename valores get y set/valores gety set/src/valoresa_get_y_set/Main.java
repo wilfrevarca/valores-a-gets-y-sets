7d945d977e0790cbb58e086df57b6f65343886e6
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-08-06 10:45:19 -0500

    Add files via upload

---
+++ valores get y set/valores gety set/src/valoresa_get_y_set/Main.java
@@ -0,0 +1,55 @@
+
+package valoresa_get_y_set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        class Persona {
+        }
+        String Nombre;
+        int Telefono;
+        int Edad;
+
+System.out.println("Nombre,Telefono,Edad");
+    }
+
+    private static void getNombre() {
+        getNombre();
+    }
+
+    private static void getTelefono() {
+        getTelefono();
+    }
+
+    private static void getEdad() {
+        getEdad();
+    }
+
+    private static void setNombre() {
+        setNombre();
+        return;
+    }
+
+    private static void setTelefono() {
+        setTelefono();
+        return;
+    }
+
+    private static void setEdad() {
+        setEdad();
+        return;
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
